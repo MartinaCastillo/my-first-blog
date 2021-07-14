f87e72320ffecf737cb99934be656a1f04b06796
MartonaEXP <martinarccorreo@gmail.com>
2021-07-14 11:14:42 -0300

    forms y cosas

---
+++ blog/scripts/main.js
@@ -0,0 +1,15 @@
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+function estableceNombreUsuario() {
+  let miNombre = prompt('Introduzca su nombre.');
+  if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es genial,' + nombreAlmacenado;
+}
+}
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
